1dcf5e8fa6b034e8e23b6fb4bbc100e6554326b1
Kevin Martin <knmartinm@gmail.com>
2020-08-13 11:57:18 -0500

    Improve show watching detection

--- js/playProperEndingTheme.js
+++ js/playProperEndingTheme.js
@@ -2,9 +2,10 @@
   const mediaName = 'Neon Genesis Evangelion'; // Works for Roman/Latin-alphabet-based languages only
   const audioBaseURL = 'https://inobtenio.dev/projects/the-ending-of-evangelion/audio/mp3/';
   let episodeNumber, episodeData, audio, video;
+  let previousURL = document.location.href;
 
   const playProperEndingTheme = function() {
-    if (watchingEvangelion()) {
+    if (watchingEvangelion() && urlHasNotChanged()) {
       episodeNumber = getEpisodeNumber();
       video = document.querySelector('video');
       audio = audio || new Audio(`${audioBaseURL}${episodeNumber}.mp3`);
@@ -17,9 +18,15 @@
 
   const watchingEvangelion = function() {
     return  document.location.href.indexOf('watch') != -1 &&
-            !document.querySelector('.player-loading') &&
             document.querySelector('.ellipsize-text') &&
+            !document.querySelector('.player-loading') &&
             document.querySelector('.ellipsize-text').textContent.indexOf(mediaName) != -1;
+  }
+
+  const urlHasNotChanged = function() {
+    const unchanged = document.location.href == previousURL;
+    previousURL = document.location.href;
+    return unchanged;
   }
 
   const getEpisodeNumber = function() {
@@ -28,30 +35,41 @@
 
   const replaceEnding = function() {
     episodeData = EPISODES[episodeNumber];
+
     if (between(video.currentTime, episodeData['muteBetween'])) {
       audio.volume = video.volume;
       video.muted = true;
 
-      video.onplay = function() {
-        if (audio) audio.play();
-      };
-
-      video.onpause = function() {
-        if (audio) audio.pause();
-      };
-
-      video.onseeked = function() {
-        seekAudio();
-      }
+      setVideoEvents();
 
       if (video.currentTime >= episodeData['endingStart']) {
         if (!endingPlaying) audio.play();
         endingPlaying = true;
       }
     } else {
+      removeVideoEvents();
+      resetAudioPlayback();
       video.muted = false;
       endingPlaying = false;
     }
+  }
+
+  const setVideoEvents = function() {
+    video.onplay = function() {
+      if (audio) audio.play();
+    };
+
+    video.onpause = function() {
+      if (audio) audio.pause();
+    };
+
+    video.onseeked = function() {
+      if (audio) seekAudio();
+    };
+  }
+
+  const removeVideoEvents = function() {
+    video.onplay = video.onpause = video.onseeked = function() {};
   }
 
   const between = function(value, range) {
@@ -61,7 +79,7 @@
   const seekAudio = function() {
     const distance = video.currentTime - episodeData['endingStart'];
 
-    if (between(distance, [0, audio.duration])) {
+    if (distance <= audio.duration) {
       audio.currentTime = distance;
     } else {
       resetAudioPlayback();
@@ -70,7 +88,7 @@
 
   const resetAudioPlayback = function() {
     audio.pause();
-    audio = undefined;
+    audio.currentTime = 0;
   }
 
   const forgetAudioAndEpisode = function() {
